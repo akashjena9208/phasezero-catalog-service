de08b47be26878cf7d71c9a84d987e3b24e46054
Akash Jena <akashjena9208@gmail.com>
2025-12-10 16:38:03 +0530

    Initial Spring Boot project generated

---
+++ src/main/java/com/phasezero/catalog/PhasezeroCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.phasezero.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhasezeroCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhasezeroCatalogServiceApplication.class, args);
+	}
+
+}
